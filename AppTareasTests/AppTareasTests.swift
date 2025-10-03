b61795f9e98d22e339c464bb97e6391f8fbbf4a1
daytona <nelson_segoviano@hotmail.com>
2025-10-02 21:15:32 -0700

    Initial Commit

---
+++ AppTareasTests/AppTareasTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTareasTests.swift
+//  AppTareasTests
+//
+//  Created by Nelson Ivan Reyes Segoviano on 02/10/25.
+//
+
+import Testing
+@testable import AppTareas
+
+struct AppTareasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
