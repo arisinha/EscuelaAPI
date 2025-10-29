b5429419cdfc49df08d356e0a4eb0e5ba2a8dfd0
daytona <nelson_segoviano@hotmail.com>
2025-10-29 12:10:18 -0700

    Fix: Clean up corrupted AppTareasUITests.swift file
    - Removed duplicate/mixed content from AppTareasUITests.swift
    - File had both old XCTest and new Swift Testing code mixed together
    - Now contains only clean Swift Testing implementation
    - Resolves 'Unable to find module dependency: XCTest' error
    - Resolves 'Unable to find module dependency: Testing' error

--- AppTareasUITests/AppTareasUITests.swift
+++ AppTareasUITests/AppTareasUITests.swift
@@ -5,23 +5,6 @@
 //  Created by Nelson Ivan Reyes Segoviano on 02/10/25.
 //
 
-import XCTest
-
-final class AppTareasUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-//
-//  AppTareasUITests.swift
-//  AppTareasUITests
-//
-//  Created by Nelson Ivan Reyes Segoviano on 02/10/25.
-//
-
 import Testing
 import XCTest // Still needed for XCUIApplication
 
@@ -45,28 +28,5 @@ struct AppTareasUITests {
         let app = XCUIApplication()
         app.launch()
         #expect(app.state == .runningForeground)
-    }
-}
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
     }
 }
